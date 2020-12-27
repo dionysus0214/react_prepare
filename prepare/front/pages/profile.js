92f16d657bc30d205145dae5b6e6725c2f0ddcbc
dionysus0214 <hiba0214@naver.com>
2020-12-27 15:35:51 +0900

    트위터 클론코딩 16강까지 완료(커스텀 훅을 활용한 회원가입 페이지 만들기)

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
+
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+  const followerList = [{nickname: '라이언'}, {nickname: '어피치'}, {nickname: '죠르디'}];
+  const followingList = [{nickname: '라이언'}, {nickname: '어피치'}, {nickname: '죠르디'}];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
