feb92629ffa1a5fdf5a9b9a47880c365e82c4352
dionysus0214 <hiba0214@naver.com>
2021-01-10 15:30:55 +0900

    트위터 클론코딩 29강까지 완료(redux-thunk 이해하기)

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -5,16 +5,56 @@ export const initialState = {
   loginData: {},
 }
 
-export const loginAction = (data) => {
+export const loginActioin = (data) => {
+  return (dispatch, getState) => {
+    const state = getState();
+    dispatch(loginRequestAction());
+    axios.post('/api/login')
+      .then((res) => {
+        dispatch(loginSuccessAction(res.data));
+      })
+      .catch((err) => {
+        dispatch(loginFailureAction(err));
+      })
+  }
+}
+
+export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN',
+    type: 'LOG_IN_REQUEST',
     data,
   }
 }
 
-export const logoutAction = () => {
+export const loginSuccessAction = (data) => {
   return {
-    type: 'LOG_OUT',
+    type: 'LOG_IN_SUCCESS',
+    data,
+  }
+}
+
+export const loginFailureAction = (data) => {
+  return {
+    type: 'LOG_IN_FAILURE',
+    data,
+  }
+}
+
+export const logoutRequestAction = () => {
+  return {
+    type: 'LOG_OUT_REQUEST',
+  }
+}
+
+export const logoutSuccessAction = () => {
+  return {
+    type: 'LOG_OUT_SUCCESS',
+  }
+}
+
+export const logoutFailureAction = () => {
+  return {
+    type: 'LOG_OUT_FAILURE',
   }
 }
 
