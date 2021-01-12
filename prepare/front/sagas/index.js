c68958e1975176099325c6b6477242c2ebf70a6a
dionysus0214 <hiba0214@naver.com>
2021-01-12 20:04:33 +0900

    트위터 클론코딩 32강까지 완료(take 시리즈, throttle 알아보기)

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,16 +1,16 @@
-import {all, call, fork, take, put} from 'redux-saga/effects';
+import {all, call, fork, takeLatest, put, delay} from 'redux-saga/effects';
 import axios from 'axios';
 
-function logInAPI() {
-  return axios.post('/api/login');
+function logInAPI(data) {
+  return axios.post('/api/login', data);
 }
 
-function* logIn() {
+function* logIn(action) {
   try {
-    const result = yield call(logInAPI);
+    // const result = yield call(logInAPI);
+    yield delay(1000);
     yield put({
       type : 'LOG_IN_SUCCESS',
-      data : result.data,
     });
   } catch(err) {
     yield put({
@@ -20,16 +20,54 @@ function* logIn() {
   }
 }
 
+function logOutAPI() {
+  return axios.post('/api/logout');
+}
+
+function* logOut() {
+  try {
+    // const result = yield call(logOutAPI);
+    yield dalay(1000);
+    yield put({
+      type : 'LOG_OUT_SUCCESS',
+    });
+  } catch(err) {
+    yield put({
+      type : 'LOG_OUT_FAILURE',
+      data : err.response.data,
+    });
+  }
+}
+
+function addPostAPI(data) {
+  return axios.post('/api/post', data);
+}
+
+function* addPost(action) {
+  try {
+    // const result = yield call(addPostAPI, action.data);
+    yield dalay(1000);
+    yield put({
+      type : 'ADD_POST_SUCCESS',
+    });
+  } catch(err) {
+    yield put({
+      type : 'ADD_POST_FAILURE',
+      data : err.response.data,
+    });
+  }
+}
+
 function* watchLogIn() {
-  yield take('LOG_IN_REQUEST', logIn);
+  yield takeLatest('LOG_IN_REQUEST', logIn);
 }
 
 function* watchLogOut() {
-  yield take('LOG_OUT_REQUEST');
+  yield takeLatest('LOG_OUT_REQUEST', logOut);
 }
 
 function* watchAddPost() {
-  yield take('ADD_POST_REQUEST');
+  yield takeLatest('ADD_POST_REQUEST', addPost);
 }
 
 export default function* rootSaga() {
