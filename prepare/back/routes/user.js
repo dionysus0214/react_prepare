d5f3779c910be048cf706cef97cf7f1bf18d98a4
dionysus0214 <hiba0214@naver.com>
2021-02-08 17:30:26 +0900

    트위터 클론코딩 74강까지 완료(swr 사용하기)

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -46,7 +46,6 @@ router.get('/followers', isLoggedIn, async (req, res, next) => {
       res.status(403).send('없는 사람을 찾으려고 하시네요?');
     }
     const followers = await user.getFollowers({
-      attributes: ['id', 'nickname'],
       limit: parseInt(req.query.limit, 10),
     });
     res.status(200).json(followers);
@@ -63,7 +62,6 @@ router.get('/followings', isLoggedIn, async (req, res, next) => {
       res.status(403).send('없는 사람을 찾으려고 하시네요?');
     }
     const followings = await user.getFollowings({
-      attributes: ['id', 'nickname'],
       limit: parseInt(req.query.limit, 10),
     });
     res.status(200).json(followings);
@@ -73,7 +71,7 @@ router.get('/followings', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
     const fullUserWithoutPassword = await User.findOne({
       where: { id: req.params.id },
