f14305d7f7f88912bc0504952755d41fc19de270
dionysus0214 <hiba0214@naver.com>
2021-02-21 11:25:31 +0900

    add BackUrl

--- prepare/front/components/PostForm.js
+++ prepare/front/components/PostForm.js
@@ -3,7 +3,8 @@ import {Form, Input, Button} from 'antd';
 import {useDispatch, useSelector} from 'react-redux';
 
 import useInput from '../hooks/useInput';
-import {addPost, UPLOAD_IMAGES_REQUEST, REMOVE_IMAGE, ADD_POST_REQUEST} from '../reducers/post';
+import {UPLOAD_IMAGES_REQUEST, REMOVE_IMAGE, ADD_POST_REQUEST} from '../reducers/post';
+import {backUrl} from '../config/config';
 
 const PostForm = () => {
   const {imagePaths, addPostDone} = useSelector((state) => state.post);
@@ -66,7 +67,7 @@ const PostForm = () => {
       <div>
         {imagePaths.map((v, i) => (
           <div key={v} style={{display: 'inline-block'}}>
-            <img src={`http://localhost:3030/${v}`} style={{width: '200px'}} alt={v} />
+            <img src={`${backUrl}/${v}`} style={{width: '200px'}} alt={v} />
             <div>
               <Button onClick={onRemoveImage(i)}>제거</Button>
             </div>
---
+++ prepare/front/config/config.js
@@ -0,0 +1,1 @@
+export const backUrl = 'http://13.209.76.127';
