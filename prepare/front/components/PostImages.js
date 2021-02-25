74810f4d8713a11704cde2ac5acfb0e5ad3dbc4a
dionysus0214 <hiba0214@naver.com>
2021-02-25 16:16:13 +0900

    backUrl

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -26,8 +26,8 @@ const PostImages = ({images}) => {
   if(images.length === 2 ) {
     return (
       <>
-        <img role="presentation" style={{width: '50%', display: 'inline-block'}} src={`http://localhost:3030/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-        <img role="presentation" style={{width: '50%', display: 'inline-block'}} src={`http://localhost:3030/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
+        <img role="presentation" style={{width: '50%', display: 'inline-block'}} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{width: '50%', display: 'inline-block'}} src={`${backUrl}/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
         {showImagesZoom && <ImagesZoom images={images} onCLose={onClose} />}
       </>
     );
@@ -35,7 +35,7 @@ const PostImages = ({images}) => {
   return (
     <>
       <div>
-        <img role="presentation" style={{width: '50%', display: 'inline-block'}} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{width: '50%'}} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
         <div
           role="presentation"
           style={{display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle'}}
